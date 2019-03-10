19ee808d0358bda1d63b5e3b4f8514f660c2bb5b
TwoEyedMikeWazowski <danielsyrov9@gmail.com>
2019-03-10 15:25:20 +0800

    Setup

--- Car Game Test/ViewController.swift
+++ Car Game Test/ViewController.swift
@@ -10,11 +10,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var gifView: UIImageView!
+    @IBOutlet weak var statsLabel: UILabel!
+    
+    var fuel = 5
+    var pts = 0
+    var time = 0
+    var timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        statsLabel.text = "POINTS: \(pts) FUEL: \(fuel)L"
+        moveCar()
     }
 
 
+    @IBAction func startButton(_ sender: Any) {
+        moveCar()
+    }
+    
+    @IBAction func stopButton(_ sender: Any) {
+        stopGame()
+    }
+    
+    func moveCar() {
+        gifView.loadGif(name: "movingroad")
+        
+        fuel = 5
+        pts = 0
+        time = 0
+        self.statsLabel.text = "POINTS: \(self.pts) FUEL: \(self.fuel)"
+        
+        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (_) in
+            self.time += 1
+            print(self.time)
+            if self.time % 5 == 0 {
+                self.pts += 50
+                self.fuel -= 1
+                self.statsLabel.text = "POINTS: \(self.pts) FUEL: \(self.fuel)"
+            }
+            
+            if self.fuel == 0 {
+                self.stopGame()
+            }
+        })
+        
+    }
+    
+    func stopGame() {
+        gifView.image = UIImage(named: "stoproad")
+        timer.invalidate()
+    }
 }
 

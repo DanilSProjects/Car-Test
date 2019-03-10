7c22b022813b099fc0bfc6ef0d6f9c1c7dae3bd2
TwoEyedMikeWazowski <danielsyrov9@gmail.com>
2019-03-10 16:54:29 +0800

    Images, trash moving down

--- Car Game Test/ViewController.swift
+++ Car Game Test/ViewController.swift
@@ -18,10 +18,14 @@ class ViewController: UIViewController {
     var time = 0
     var timer = Timer()
     
+    @IBOutlet weak var trash1: UIImageView!
+    @IBOutlet weak var trash2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         statsLabel.text = "POINTS: \(pts) FUEL: \(fuel)L"
         moveCar()
+        
     }
 
 
@@ -39,6 +43,8 @@ class ViewController: UIViewController {
         fuel = 5
         pts = 0
         time = 0
+        trash1.isHidden = true
+        trash2.isHidden = true
         self.statsLabel.text = "POINTS: \(self.pts) FUEL: \(self.fuel)"
         
         timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (_) in
@@ -48,6 +54,21 @@ class ViewController: UIViewController {
                 self.pts += 50
                 self.fuel -= 1
                 self.statsLabel.text = "POINTS: \(self.pts) FUEL: \(self.fuel)"
+            }
+            
+            if self.time % 2 == 0 {
+                self.trash1.isHidden = false
+                self.trash2.isHidden = false
+                
+                UIView.animate(withDuration: 1, animations: {
+                    self.trash1.frame.origin.y += 300
+                    self.trash2.frame.origin.y += 300
+                }, completion: { (_) in
+                    self.trash1.isHidden = true
+                    self.trash2.isHidden = true
+                    self.trash1.frame.origin.y = 203.0
+                    self.trash2.frame.origin.y = 203.0
+                })
             }
             
             if self.fuel == 0 {
